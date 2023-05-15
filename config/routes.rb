c77885b6eff2ddf1962fad18543bde10c9605938
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-15 08:07:30 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about",as:"about"
+
+  namespace :admin do
+    resources :sessions,only:[:new,:create,:destroy]
+  end
+end
